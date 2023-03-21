da03c1af0a37ccb8b12ba7d0fd81aa8529c95566
xmgg <lxmliu2002@126.com>
2023-03-21 21:36:32 +0800

    第一次实验

---
+++ 1/20230307/20230307.srcs/sim_1/new/testbench.v
@@ -0,0 +1,44 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2023/03/07 19:35:29
+// Design Name: 
+// Module Name: testbench
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module testbench();
+reg [31:0] op1,op2;
+reg op3;
+wire [31:0] sum;
+wire op_1,op_2,op_3,op4;
+
+adder8 a7_0(op1[7:0],op2[7:0],op3,sum[7:0],op_1);
+adder8 a15_8(op1[15:8],op2[15:8],op_1,sum[15:8],op_2);
+adder8 a23_16(op1[23:16],op2[23:16],op_2,sum[23:16],op_3);
+adder8 a31_24(op1[31:24],op2[31:24],op_3,sum[31:24],op4);
+
+initial
+begin
+    op1=32'b0;
+    op2=32'b0;
+    op3=1'b0;
+end
+
+always #3 op1 = $random % 33'b1_0000_0000_0000_0000_0000_0000_0000_0000;
+always #5 op2 = $random % 33'b1_0000_0000_0000_0000_0000_0000_0000_0000;
+always #7 op3 = $random % 2'b1_0;
+
+endmodule
---
+++ 1/20230307/20230307.srcs/sources_1/new/adder32.v
@@ -0,0 +1,42 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2023/03/14 22:35:01
+// Design Name: 
+// Module Name: adder32
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module adder32(
+    input [31:0] ina1,
+    input [31:0] inb1,
+    input inc1,
+    output [31:0] sum1,
+    output outc1
+    );
+    reg [31:0] op1,op2;
+    reg op3;
+    wire [31:0] sum;
+    wire op_1,op_2,op_3,op4;
+
+    adder8 a7_0(op1[7:0],op2[7:0],op3,sum[7:0],op_1);
+    adder8 a15_8(op1[15:8],op2[15:8],op_1,sum[15:8],op_2);
+    adder8 a23_16(op1[23:16],op2[23:16],op_2,sum[23:16],op_3);
+    adder8 a31_24(op1[31:24],op2[31:24],op_3,sum[31:24],op4);
+    
+    assign {outc1,sum1} = ina1 + inb1 + inc1;
+    
+endmodule
